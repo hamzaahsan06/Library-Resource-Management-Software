5893c6ac1c9a5a8aeabb1baaf96bb29049205920
Khancoders <muhammedmoosaiqbal3@gmail.com>
2026-05-09 17:13:42 +0500

    Added Cmake

---
+++ build/CMakeFiles/LibrarySystem.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "C:/Users/moosa/OneDrive/Desktop/Library-Resource-Management-Software/bin/LibrarySystem.exe"
+  "C:/Users/moosa/OneDrive/Desktop/Library-Resource-Management-Software/bin/LibrarySystem.exe.manifest"
+  "C:/Users/moosa/OneDrive/Desktop/Library-Resource-Management-Software/bin/LibrarySystem.pdb"
+  "CMakeFiles/LibrarySystem.dir/main.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/main.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/Admin.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/Admin.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/FileHandler.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/FileHandler.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/Library.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/Library.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/Menu.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/Menu.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/Resources.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/Resources.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/Title.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/Title.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/Users.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/Users.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/Utils.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/Utils.cpp.obj.d"
+  "libLibrarySystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibrarySystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
